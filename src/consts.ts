29d17aa2779ebcc585ec9ad95ff8c129a249a160
Dick Bevernage <dick_bevernage@hotmail.com>
2026-09-20 13:43:07 +0200

    chore(UserAnalytics): intro Umami tracking id

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const CONTACT_EMAIL = 'sporaleuven@hotmail.com';
-export const UMAMI_WEBSITE_ID = import.meta.env.PUBLIC_UMAMI_WEBSITE_ID;
-export const UMAMI_SCRIPT_URL =
+export const UMAMI_WEBSITE_ID = 'b43766d6-f121-4a8e-bdef-3bbc39bbd9a4';
+export const UMAMI_SCRIPT_URL = 
   import.meta.env.PUBLIC_UMAMI_SCRIPT_URL || 'https://cloud.umami.is/script.js';
